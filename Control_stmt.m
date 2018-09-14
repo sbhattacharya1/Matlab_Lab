87fc312c011c07af72b1108a7b1c6a8d65fd0256
Sarani Bhattacharya <42635843+sbhattacharya1@users.noreply.github.com>
2018-09-14 17:46:01 +0530

    Add files via upload

---
+++ Control_stmt.m
@@ -0,0 +1,30 @@
+v=zeros(10,1);
+z=1;
+%If loop syntax
+if z == 2 
+    disp("Z is 2");
+elseif z == 1
+    disp("Z is 1");
+else
+    disp("Z is not 1 or 2");
+end;
+
+%For Loop syntax
+for i=1:10
+    v(i)=2^i;
+end;
+
+%For loop with -ve step
+for i=10:-1:1
+    disp(v(i));
+end;
+
+%While Loop Syntax
+i=1;
+while i<11
+    disp(i);
+    i=i+1;
+    if i == 7
+        break;              %Break statement to break while loop
+    end;
+end;
