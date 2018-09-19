b6ecda2e62371f73af8edc4c99bf1a2d8a36366e
Sarani Bhattacharya <42635843+sbhattacharya1@users.noreply.github.com>
2018-09-19 20:41:33 +0530

    Update plotting_examples.m

--- plotting_examples.m
+++ plotting_examples.m
@@ -50,21 +50,21 @@ figure
 plot3(st,ct,t)                                          %function for 3-D plotting
 
 
-Various line types, plot symbols and colors may be obtained with
-    plot(X,Y,S) where S is a character string made from one element
-    from any or all the following 3 columns:
-             b     blue          .     point              -     solid
-             g     green         o     circle             :     dotted
-             r     red           x     x-mark             -.    dashdot 
-             c     cyan          +     plus               --    dashed   
-             m     magenta       *     star             (none)  no line
-             y     yellow        s     square
-             k     black         d     diamond
-             w     white         v     triangle (down)
-                                 ^     triangle (up)
-                                 <     triangle (left)
-                                 >     triangle (right)
-                                 p     pentagram
-                                 h     hexagram
-
+%Various line types, plot symbols and colors may be obtained with
+%    plot(X,Y,S) where S is a character string made from one element
+%    from any or all the following 3 columns:
+%             b     blue          .     point              -     solid
+%             g     green         o     circle             :     dotted
+%             r     red           x     x-mark             -.    dashdot 
+%             c     cyan          +     plus               --    dashed   
+%             m     magenta       *     star             (none)  no line
+%             y     yellow        s     square
+%             k     black         d     diamond
+%             w     white         v     triangle (down)
+%                                 ^     triangle (up)
+%                                 <     triangle (left)
+%                                 >     triangle (right)
+%                                 p     pentagram
+%                                 h     hexagram
+%
 
